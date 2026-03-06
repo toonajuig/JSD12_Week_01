b040d08abda31edfae339af0c8ead1242115ba00
toonajuig <thirawat.devtt@gmail.com>
2026-03-06 14:03:39 +0700

    added JS Json and clone

---
+++ object-and-array.js
@@ -0,0 +1,19 @@
+const learner = {
+    name: "Thirawat Rojanakul",
+    age: 32,
+    graduated: true,
+    hobbies: ["running", "gaming", "weight training"]
+};
+console.log("Name:", learner.name);
+console.log("Age:", learner.age);
+console.log("Graduated:", learner.graduated);
+console.log("First hobby:", learner.hobbies[0]);
+
+
+
+
+
+
+
+
+
